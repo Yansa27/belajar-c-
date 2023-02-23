b80c3e74e46f4b95db84339a0f9337dd3083207e
Yansa27 <juliansa837@gmail.com>
2023-02-23 22:52:22 +0700

    push

--- Belajar.cpp
+++ Belajar.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main () {
+int main()
+{
 	cout << "========== PROGRAM OUTPUT ==========" << endl;
 	cout << "Hallo nama saya Juliansa \n";
 	cout << "Saya sedang belajar C++\n";
